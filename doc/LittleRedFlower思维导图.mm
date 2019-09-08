2eea0481245deed4074988543c28ee50c77af723
ELE-Clouds <gt_china_bird@126.com>
2019-09-09 00:44:22 +0800

    V0.03版修改增加游戏功能

---
+++ doc/LittleRedFlower思维导图.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="LittleRedFlower" FOLDED="false" ID="ID_62282499" CREATED="1567607575807" MODIFIED="1567607592877" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#x79ef;&#x5206;&#x529f;&#x80fd;" POSITION="right" ID="ID_1509178738" CREATED="1567607606176" MODIFIED="1567607623126">
+<edge COLOR="#ff0000"/>
+<node TEXT="&#x5956;&#x52b1;&#x4e00;&#x6735;&#x5c0f;&#x7ea2;&#x82b1;&#x83b7;&#x5f97;1&#x70b9;&#x79ef;&#x5206;" ID="ID_628984020" CREATED="1567607812508" MODIFIED="1567607826187"/>
+<node TEXT="&#x79ef;&#x5206;&#x5927;&#x4e8e;100&#x53ef;&#x6fc0;&#x6d3b;&#x9690;&#x85cf;&#x6e38;&#x620f;" ID="ID_1232631810" CREATED="1567607826927" MODIFIED="1567607842799"/>
+<node TEXT="&#x6bcf;&#x767b;&#x9646;&#x4e00;&#x6b21;&#x6e38;&#x620f;&#x9700;&#x8981;&#x6d88;&#x8017;&#x4e00;&#x5b9a;&#x6570;&#x91cf;&#x7684;&#x79ef;&#x5206;" ID="ID_526644342" CREATED="1567607843209" MODIFIED="1567607862809"/>
+</node>
+<node TEXT="&#x5956;&#x52b1;&#x529f;&#x80fd;" POSITION="right" ID="ID_1010270526" CREATED="1567607647546" MODIFIED="1567607657476">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#x7528;&#x6237;A&#x83b7;&#x5f97;&#x4e00;&#x6735;&#x5c0f;&#x7ea2;&#x82b1;" ID="ID_198533823" CREATED="1567607661066" MODIFIED="1567607709497"/>
+<node TEXT="&#x7528;&#x6237;B&#x83b7;&#x5f97;&#x4e00;&#x6735;&#x5c0f;&#x7ea2;&#x82b1;" ID="ID_1602041572" CREATED="1567607691682" MODIFIED="1567607703277"/>
+<node TEXT="&#x79ef;&#x5206;&#xff08;&#x5c0f;&#x7ea2;&#x82b1;&#x6570;&#x91cf;&#xff09;&#x5927;&#x4e8e;100&#x53ef;&#x6fc0;&#x6d3b;&#x9690;&#x85cf;&#x6e38;&#x620f;&#x529f;&#x80fd;" ID="ID_1132845914" CREATED="1567607720248" MODIFIED="1567607754660"/>
+<node TEXT="&#x6bcf;&#x767b;&#x9646;&#x4e00;&#x6b21;&#x6e38;&#x620f;&#x9700;&#x8981;&#x6d88;&#x8017;&#x4e00;&#x5b9a;&#x6570;&#x91cf;&#x7684;&#x79ef;&#x5206;&#xff08;&#x5c0f;&#x7ea2;&#x82b1;&#xff09;" ID="ID_776335184" CREATED="1567607755130" MODIFIED="1567607794318"/>
+</node>
+<node TEXT="&#x6e38;&#x620f;&#x529f;&#x80fd;" POSITION="right" ID="ID_952233593" CREATED="1567607623726" MODIFIED="1567608236260">
+<edge COLOR="#0000ff"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21482;&#26377;&#20004;&#20010;&#29992;&#25143;&#21516;&#26102;&#28385;&#36275;&#28216;&#25103;&#28608;&#27963;&#35201;&#27714;&#65288;&#31215;&#20998;&#22823;&#20110;100&#65289;&#25165;&#21487;&#20197;&#36827;&#34892;&#21452;&#20154;&#28216;&#25103;&#65292;&#21542;&#21017;&#21482;&#33021;&#36827;&#34892;&#21333;&#20154;&#28216;&#25103;&#65292;&#23545;&#30001;&#36719;&#20214;&#20195;&#30721;&#26367;&#20195;&#12290;
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&#x63b7;&#x8272;&#x5b50;" ID="ID_1242686362" CREATED="1567607870424" MODIFIED="1567607877868"/>
+<node TEXT="&#x77f3;&#x5934;&#x526a;&#x5200;&#x5e03;" ID="ID_1847898822" CREATED="1567607878328" MODIFIED="1567607887248"/>
+<node TEXT="&#x65cb;&#x8f6c;&#x8f6e;&#x76d8;" ID="ID_444311088" CREATED="1567607887666" MODIFIED="1567607898636"/>
+</node>
+</node>
+</map>
